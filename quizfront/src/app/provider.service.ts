238bfb808a68030b0deb69374af617579efb583a
Moldash Syrym <solomon.001412@gmail.com>
2019-04-29 23:15:31 +0600

    Second commit

---
+++ quizfront/src/app/provider.service.ts
@@ -0,0 +1,21 @@
+import {Injectable} from '@angular/core';
+import {MainService} from './main.service';
+import {HttpClient} from '@angular/common/http';
+import {ITask, ITaskList} from '../models/models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProviderService extends MainService {
+
+  constructor(http: HttpClient) {
+    super(http);
+  }
+
+  getTaskLists(): Promise<ITaskList[]> {
+    return this.get('http://127.0.0.1:8000/api/task_lists', {});
+  }
+
+
+
+}
